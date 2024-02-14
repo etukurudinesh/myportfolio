a6cd70e9f3c62cdbb12641f7d2b6dd7bf38ef8c7
dinesh.etukuru <dinesh.etukuru@deptagency.com>
2024-02-14 01:36:03 +0000

    Adding file for the first time to git

---
+++ myportfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-9fe3jy70qq)mv)auo8jv8@b8+x_t@slhe66f*c)&ab(w7dnme&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
